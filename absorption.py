03df287cc70c3915babfa969a606d45016f844e1
rnegrao <renatonegraooliveira@gmail.com>
2026-09-10 08:15:27 -0300

    Refactor absorption.py
    Refactor absorption.py

--- absorption.py
+++ absorption.py
@@ -12,14 +12,14 @@ maker_style = [
                 '^'
                 ]
 hex_colors_bw = [
-        "#565656",
-        "#6B6B6B",
-        "#808080",
-        "#969696",
-        "#ACACAC",
-        "#C2C2C2",
-        "#D8D8D8"
-                ]
+    "#565656",
+    "#6B6B6B",
+    "#808080",
+    "#969696",
+    "#ACACAC",
+    "#C2C2C2",
+    "#D8D8D8"
+            ]
 hex_colors = [
     "#67000D",
     "#A50F15",
@@ -31,15 +31,15 @@ hex_colors = [
     "#FEE0D2"
     ]
 hex_colors_blue = [
-            "#08306b",
-            "#08519c",
-            "#2171b5",
-            "#4292c6",
-            "#6baed6",
-            "#9ecae1",
-            "#c6dbef",
-            "#deebf7"
-            ]
+    "#08306b",
+    "#08519c",
+    "#2171b5",
+    "#4292c6",
+    "#6baed6",
+    "#9ecae1",
+    "#c6dbef",
+    "#deebf7"
+    ]
 
 if __name__ == '__main__':
 
@@ -70,9 +70,7 @@ if __name__ == '__main__':
         'lines.linewidth': 1.5,
         'lines.markersize': 6,
         'mathtext.fontset': 'cm',
-    })
-
-    
+    })  
     
     # plot data
     fig1, ax = plt.subplots(figsize=(8, 8))
@@ -83,11 +81,9 @@ if __name__ == '__main__':
     
     ax.plot(lambda_ang,Helium3, label='3He 20atm and 0.5cm thickness. Data points from NIST', marker=maker_style[1],linestyle='None',color=hex_colors_bw[1])
     ax.plot(lambda_ang,LiF, label='6LiF:ZnS(Ag) 250$\mu$m thickness. Data points from NIST',marker=maker_style[2],linestyle='None',color=hex_colors_bw[2])
-    ax.plot(lambda_ang_1,Helium3_1, label='3He 20atm. Data points from ASI Quotation',marker=maker_style[3],linestyle='None',color=hex_colors_bw[3])
+    ax.plot(lambda_ang_1,Helium3_1, label='3He 20atm. Data points from ASI Quotation',marker=maker_style[0],linestyle='None',color=hex_colors_bw[3])
     ax.plot(lambda_ang_2,LiF_2, label='6LiF:ZnS(Ag) 250$\mu$m thickness. Data points from DOI',marker=maker_style[4],linestyle='None',color=hex_colors[2])
     ax.plot(lambda_ang_2,LiI_2, label='6LiI:Eu 250$\mu$m thickness. Data points from DOI',marker=maker_style[5],linestyle='None',color=hex_colors[3])
-
-    
 
     # Add labels and title
     plt.xlabel('$\lambda$ (Ang)')
